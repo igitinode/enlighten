b041939ad12453922dc54ff7387c8e4d44e2dedb
igitinode <igitinode@gmail.com>
2025-01-10 22:11:08 +0800

    algolia：修改 Index 的名字

--- docs/.vitepress/config/algolia.mjs
+++ docs/.vitepress/config/algolia.mjs
@@ -1,7 +1,7 @@
 export default {
   appId: 'EH1CQR29YE',
   apiKey: 'ca8ad0c2f9df58e28d28d8bf339203da',
-  indexName: 'igitinode_github_io_eh1cqr29ye_pages',
+  indexName: 'Enlighten',
   placeholder: '搜索文档',
   translations: {
     button: {
